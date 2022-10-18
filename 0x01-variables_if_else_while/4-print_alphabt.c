1eb7f42d3bacde3723376a2674f452c6bd439e05
Attigah <attigahbernard6@gmail.com>
2022-10-17 22:10:06 -0700

    èyesahè

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ *  * main - start of program
+ *   * Return: has a value of 0
+ *    */
+int main(void)
+{
+	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+	i = 0;
+	for (; i < 26;)
+	{
+		char ch = alphabet[i];
+		if (i == 4 || i == 16)
+		{
+		}
+		else
+		{
+			putchar(ch);
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+nclude <stdio.h>
+
+/**
+ *  * main - start of program
+ *   * Return: has a value of 0
+ *    */
+int main(void)
+{
+	int i;
+	int end;
+	i = 48;
+	end = 58;
+	for (; i < end;)
+	{
+		putchar(i);
+		if (i < 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
