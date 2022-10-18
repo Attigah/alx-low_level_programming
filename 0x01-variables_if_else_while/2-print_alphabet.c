6e8e0c7dd834ccaa2df882e82a021f89c19674d5
Attigah <attigahbernard6@gmail.com>
2022-10-17 21:57:09 -0700

    èyesè

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,17 +9,17 @@ int main(void)
 {
 	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
-	i = 0;
 
+	i = 0;
 	for (; i < 26;)
 
 	{
 		char ch = alphabet[i];
 		i++;
-
 		putchar(ch);
 	}
 
 	putchar('\n');
+
 	return (0);
 }
