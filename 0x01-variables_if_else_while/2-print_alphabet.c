e657e9fac48423abcccf9c219bdfc08162e4bbe6
Attigah <attigahbernard6@gmail.com>
2022-10-17 21:51:12 -0700

    èyeahè

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
 
-/**
+/*
  *  * main - start of program
  *   * Return: has a value of 0
- *    */
+ */
+
 int main(void)
 {
 	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
 	i = 0;
 	for (; i < 26;)
+
 	{
 		char ch = alphabet[i];
 		i++;
 		putchar(ch);
 	}
+
 	putchar('\n');
 	return (0);
 }
